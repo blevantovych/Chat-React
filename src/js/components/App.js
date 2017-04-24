93e97606429d8aa8af21c68c90ef0a8a09cf9810
bodya17 <levantovychbogdan@gmail.com>
2017-04-24 17:04:32 +0300

    add basic files

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                This is Chat
+            </div>
+        );
+    }
+}
+
+export default App;
